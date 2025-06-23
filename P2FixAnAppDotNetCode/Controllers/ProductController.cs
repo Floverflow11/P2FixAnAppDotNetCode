6b887bb35143b9e0c3254ca5a79c361a18bfb1f3
Floverflow <flo.cinbolat@gmail.com>
2025-06-23 11:55:32 +0200

    refactor: change return type from array to list to get all products from the inventory

--- P2FixAnAppDotNetCode/Controllers/ProductController.cs
+++ P2FixAnAppDotNetCode/Controllers/ProductController.cs
@@ -17,7 +17,7 @@ namespace P2FixAnAppDotNetCode.Controllers
 
         public IActionResult Index()
         {
-            Product[] products = _productService.GetAllProducts();
+            var products = _productService.GetAllProducts();
             return View(products);
         }
     }
